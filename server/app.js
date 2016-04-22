40008a321a2c19cafe520a937bbf18883bd313cc
Joshua Sullivan <josh@Joshuas-MacBook-Pro.local>
2016-04-22 11:03:45 -0600

    fixing some of the gulp stuff so you don't run into race conditions when running clean

--- server/app.js
+++ server/app.js
@@ -12,8 +12,7 @@
   var app = express();
   //comment
   app.set('views', path.join(__dirname, 'views'));
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
+  app.set('view engine', 'hbs');
 
   app.use(logger('dev'));
   app.use(bodyParser.json());
