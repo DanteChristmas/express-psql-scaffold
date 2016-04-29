67d5d89b27104ffe0c1a76729c9ed75e16c07130
Joshua Sullivan <josh@Joshuas-MacBook-Pro.local>
2016-04-29 14:32:43 -0600

    basic ui-router implementation

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 angular.module('templates', []);
-angular.module('reddit-clone', ['ui.router', 'templates']);
+angular.module('reddit-clone', ['templates'])
+  .run(function($http, $log) {
+    $http.jsonp('http://ksu-oas-web-int.slabs-cloud.com/v3/sports')
+    .then(function (data) {
+      $log.info(data.data);
+    });
+  });
